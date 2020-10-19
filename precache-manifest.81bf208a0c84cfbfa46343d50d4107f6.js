b04bdb2ca986bbd13f36de5fb4d28ac14e49117d
Gregory Bai <gregorybai@outlook.com>
2020-10-19 16:05:10 +0300

    Updates

---
+++ precache-manifest.81bf208a0c84cfbfa46343d50d4107f6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8db8f0847a93a69f82dc3124c77bd3",
+    "url": "/react-figma-task/index.html"
+  },
+  {
+    "revision": "a88bc7fbecb3b744b6a9",
+    "url": "/react-figma-task/static/css/main.3b799561.chunk.css"
+  },
+  {
+    "revision": "e99236fecb1471a5b44a",
+    "url": "/react-figma-task/static/js/2.c55cfbbb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-figma-task/static/js/2.c55cfbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88bc7fbecb3b744b6a9",
+    "url": "/react-figma-task/static/js/main.1d1f9364.chunk.js"
+  },
+  {
+    "revision": "f287a4a98b788a9ec1b4",
+    "url": "/react-figma-task/static/js/runtime-main.35639d4f.js"
+  },
+  {
+    "revision": "44a35843c5c5c0dc998a0258ea2a7740",
+    "url": "/react-figma-task/static/media/Exo2.0-Thin.44a35843.ttf"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/react-figma-task/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "d2f778640e2380b9d229bb1cb4d4ac09",
+    "url": "/react-figma-task/static/media/Exo2.0-Thin.d2f77864.woff"
+  },
+  {
+    "revision": "d479314e3c9aa85ccbd24ef14841d9d0",
+    "url": "/react-figma-task/static/media/Pattern@2x.d479314e.png"
+  },
+  {
+    "revision": "05fbcccd789629e303b4dfbf507eb5ca",
+    "url": "/react-figma-task/static/media/TrebuchetMS.05fbcccd.woff"
+  },
+  {
+    "revision": "b00dad0cd6f5891361dafbe8f28af2ba",
+    "url": "/react-figma-task/static/media/TrebuchetMS.b00dad0c.otf"
+  },
+  {
+    "revision": "c8fb4d1637e8b48f8f286e283073aa35",
+    "url": "/react-figma-task/static/media/TrebuchetMS.c8fb4d16.ttf"
+  },
+  {
+    "revision": "d9c47d815a0f5fd4a6e1f219a48c4240",
+    "url": "/react-figma-task/static/media/card-back-default-hover.d9c47d81.svg"
+  },
+  {
+    "revision": "d9c47d815a0f5fd4a6e1f219a48c4240",
+    "url": "/react-figma-task/static/media/card-back-disabled.d9c47d81.svg"
+  },
+  {
+    "revision": "d9c47d815a0f5fd4a6e1f219a48c4240",
+    "url": "/react-figma-task/static/media/card-back-selected.d9c47d81.svg"
+  },
+  {
+    "revision": "683228f1eca1dfb96705c2cd021ff851",
+    "url": "/react-figma-task/static/media/card-back.683228f1.svg"
+  },
+  {
+    "revision": "b91f57f0ccbea37ecfb576d031c40a23",
+    "url": "/react-figma-task/static/media/card-border-default-hover.b91f57f0.svg"
+  },
+  {
+    "revision": "8293c8589f17ad4fe6069d67236c5465",
+    "url": "/react-figma-task/static/media/card-border-default.8293c858.svg"
+  },
+  {
+    "revision": "747f3ecff8b581190283f37f45142b6f",
+    "url": "/react-figma-task/static/media/card-border-disabled.747f3ecf.svg"
+  },
+  {
+    "revision": "d178d6661cdff96e69292946c70bd606",
+    "url": "/react-figma-task/static/media/card-border-selected-hover.d178d666.svg"
+  },
+  {
+    "revision": "d178d6661cdff96e69292946c70bd606",
+    "url": "/react-figma-task/static/media/card-border-selected.d178d666.svg"
+  },
+  {
+    "revision": "e1df4cdfb434f1d8039ac71501511531",
+    "url": "/react-figma-task/static/media/card-cat-photo-disabled.e1df4cdf.png"
+  },
+  {
+    "revision": "ff0b5af19052795de2040d2c2d8eb166",
+    "url": "/react-figma-task/static/media/card-cat-photo.ff0b5af1.png"
+  }
+]);
